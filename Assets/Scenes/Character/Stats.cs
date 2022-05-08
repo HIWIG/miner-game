14d0de4044d22cba0fc4b6c78da3a62d8f8c4b34
Omush <58073254+Omush@users.noreply.github.com>
2022-05-08 13:27:40 +0200

    UpdateStats
    update

--- Assets/Scenes/Character/Stats.cs
+++ Assets/Scenes/Character/Stats.cs
@@ -10,7 +10,7 @@ public class Stats : MonoBehaviour
     private float maxHealth = 100;
     private float currentHealth = 100;
     private float maxMoney = 1000;
-    private float currentMoney=0
+    private float currentMoney = 0;
     // Start is called before the first frame update
     void Start()
     {
