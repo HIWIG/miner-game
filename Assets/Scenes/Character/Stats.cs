ce055491d9872ae99be8e00fdb67fee440a11b18
Omush <58073254+Omush@users.noreply.github.com>
2022-05-08 13:13:27 +0200

    poczatkiStatów
    dodanie podstawowego zdrowia i miejsca na pieni adze

---
+++ Assets/Scenes/Character/Stats.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public Texture2D healthTexture;
+    public Texture2D moneyTexture;
+
+    private float maxHealth = 100;
+    private float currentHealth = 100;
+    private float maxMoney = 1000;
+    private float currentMoney=0
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
