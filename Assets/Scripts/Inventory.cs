2fdd805138511f47624674402bade819a6bfe139
HIWIG <grzesiufab@gmail.com>
2022-06-13 17:50:18 +0200

    Revert "Merge"
    This reverts commit 76ce128f873a94498fef7939d11cd3975406a963, reversing
    changes made to 751b261dd13a1a3cc80c7b0010466e2c006d9898.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Inventory : MonoBehaviour
+[CreateAssetMenu(fileName = "Inventory", menuName = "Inventory")]
+public class Inventory : ScriptableObject
 {
-<<<<<<< HEAD
     
     public int mineral;
 
@@ -13,129 +12,5 @@ public class Inventory : MonoBehaviour
     {
        
         mineral++;
-=======
-    public int stackLimit = 4;
-    public ToolClass tool;
-
-    public Vector2 offset;
-    public Vector2 multiplier; 
-
-    public GameObject inventoryUI;
-    public GameObject inventorySlotPrefab;
-
-    public int inventoryWidth;
-    public int inventoryHeight;
-    public InventorySlot[,] inventorySlots;
-    public GameObject[,] uiSlots;
-
-    public void Start()
-    {
-        inventorySlots = new InventorySlot[inventoryWidth, inventoryHeight];       
-        uiSlots = new GameObject[inventoryWidth, inventoryHeight];
-        SetupUI();
-        UpdateInventoryUI();
-        Add(new ItemClass(tool));
-    }
-
-    void SetupUI()
-    {
-        for (int x = 0; x < inventoryWidth; x++)
-        {
-            for (int y = 0; y < inventoryHeight; y++)
-            {
-                GameObject inventorySlot = Instantiate(inventorySlotPrefab, inventoryUI.transform.GetChild(0).transform);
-                inventorySlot.GetComponent<RectTransform>().localPosition = 
-                    new Vector2((x * multiplier.x) + offset.x, (y * multiplier.y) + offset.y);
-                uiSlots[x, y] = inventorySlot;
-
-                inventorySlots[x, y] = null;
-            }
-        }
-    }
-    void UpdateInventoryUI()
-    {
-        for (int x = 0; x < inventoryWidth; x++)
-        {
-            for (int y = 0; y < inventoryHeight; y++)
-            {
-                if (inventorySlots[x, y] == null)
-                {
-                    uiSlots[x, y].transform.GetChild(0).GetComponent<Image>().sprite = null;
-                    uiSlots[x, y].transform.GetChild(0).GetComponent<Image>().enabled = false;
-
-                    uiSlots[x, y].transform.GetChild(1).GetComponent<Text>().text = "0";
-                    uiSlots[x, y].transform.GetChild(1).GetComponent<Text>().enabled = false;
-                }
-                else
-                {
-                    uiSlots[x, y].transform.GetChild(0).GetComponent<Image>().enabled = true;
-                    uiSlots[x, y].transform.GetChild(0).GetComponent<Image>().sprite = inventorySlots[x,y].item.sprite;
-
-                    uiSlots[x, y].transform.GetChild(1).GetComponent<Text>().text = inventorySlots[x, y].quantity.ToString();
-                    uiSlots[x, y].transform.GetChild(1).GetComponent<Text>().enabled = true;
-                }
-            }
-        } 
-    }
-    public bool Add(ItemClass item)
-    {
-        bool itemAdded = false;
-        Vector2Int itemPos = Contains(item);
-        if (itemPos != Vector2Int.one * -1)
-        {
-            if (inventorySlots[itemPos.x, itemPos.y].quantity < stackLimit)
-            {
-                inventorySlots[itemPos.x, itemPos.y].quantity++;
-                itemAdded = true;
-            }
-        }
-        if(!itemAdded)
-        {
-            for (int y = inventoryHeight - 1; y >= 0; y--)
-            {
-                if (itemAdded) break;
-                for (int x = 0; x < inventoryWidth; x++)
-                {
-                    if (inventorySlots[x, y] == null)
-                    {
-                        inventorySlots[x, y] = new InventorySlot
-                        {
-                            item = item,
-                            position = new Vector2Int(x, y),
-                            quantity = 1
-                        };
-                        itemAdded = true;
-                        break;
-                    }
-                }
-            }
-        }
-        UpdateInventoryUI();
-        return itemAdded; 
-    }
-
-    public Vector2Int Contains(ItemClass item)
-    {
-        for (int y = inventoryHeight - 1; y >= 0; y--)
-        {
-            for (int x = 0; x < inventoryWidth; x++)
-            {
-                if (inventorySlots[x, y] != null)
-                {
-                    if (inventorySlots[x, y].item.sprite == item.sprite)
-                    {
-                        return new Vector2Int(x, y);
-                    }
-                }
-            } 
-        }
-        
-        return Vector2Int.one * -1;
-    }
-
-    public void Remove(ItemClass item)
-    {
-
->>>>>>> main
     }
 }
--- Assets/Scripts/TileDropController.cs
+++ Assets/Scripts/TileDropController.cs
@@ -4,23 +4,13 @@ using UnityEngine;
 
 public class TileDropController : MonoBehaviour
 {
-<<<<<<< HEAD
     public void OnTriggerEnter2D(Collider2D collision)
-=======
-    public ItemClass item;
-
-    private void OnTriggerEnter2D(Collider2D collision)
->>>>>>> main
     {
         if(collision.gameObject.CompareTag("Player"))
         {
+            Destroy(this.gameObject);
             //dodawanie mineralu do ekwipunku gracza
-<<<<<<< HEAD
             
-=======
-            if(collision.GetComponent<Inventory>().Add(item))
-                Destroy(this.gameObject);
->>>>>>> main
         }
     }
 }
